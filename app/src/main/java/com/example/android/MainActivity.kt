73ac62d48353bffc528d3b76dc03662f12632c75
Yoon0221 <108733746+Yoon0221@users.noreply.github.com>
2024-10-16 20:09:21 +0900

    feat : mission_1

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.android
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        stampChicker(R.id.imageView2, "happy")
+        stampChicker(R.id.imageView3, "interest")
+        stampChicker(R.id.imageView5, "nomal")
+        stampChicker(R.id.imageView6, "nervous")
+        stampChicker(R.id.imageView7, "angry")
+
+    }
+
+    fun stampChicker (stampImage : Int, name : String) {
+        findViewById<ImageView>(stampImage).setOnClickListener {
+            todayFeeling(name)
+        }
+    }
+
+    fun todayFeeling (stamp : String) {
+        val intent = Intent(this, SubActivity::class.java)
+        intent.putExtra("feeling", stamp)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/android/SubActivity.kt
@@ -0,0 +1,26 @@
+package com.example.android
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sub)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val feeling = intent.getStringExtra("feeling")
+        Log.d("액티비티 전환", "feeling = $feeling")
+        findViewById<TextView>(R.id.textView9).text = feeling
+    }
+}
